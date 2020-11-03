53ce16f7bcca4a68260bc020e0d6acb6549607df
mbatuhantur <60035091+mbatuhantur@users.noreply.github.com>
2020-11-03 10:38:07 +0300

    Add files via upload

---
+++ araba1/araba/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace araba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ileri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 92);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "geri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 110);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "sol";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(532, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 110);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "sag";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(259, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(242, 92);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(127, 418);
+            this.trackBar1.Maximum = 90;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(544, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(584, 322);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 63);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "ARACA BAĞLAN";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 471);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 516);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ araba1/araba/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Data.SqlClient;
+
+
+namespace araba
+{
+    public partial class Form1 : Form
+    {
+        
+        SerialPort deneme = new SerialPort("COM3", 9600);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            deneme.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            deneme.Write("u");
+            
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=WINDOWS81SL\\SQLEXPRESS; Initial Catalog=araba; Integrated Security=true ");
+    
+    private void Form1_Load(object sender, EventArgs e)
+        {
+            baglan.Open();
+            label1.Text = DateTime.Today.ToShortDateString();
+
+            string kayit1 = "INSERT INTO araba.dbo.giris(tarih) Values (@tarih)";
+            SqlCommand komut1 = new SqlCommand(kayit1, baglan);
+
+            komut1.Parameters.AddWithValue("@tarih", Convert.ToDateTime(label1.Text));
+            komut1.ExecuteNonQuery();
+            baglan.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+            deneme.Write("b");
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            deneme.Write("l");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            deneme.Write("r");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            deneme.Write("s");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
